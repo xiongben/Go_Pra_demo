9463ea054444f1112a4c59eb2c57ab35d47e7b13
Xiong Ben <xiongben1993@gmail.com>
2020-02-17 16:12:38 +0800

    [add]

--- demo1/main.go
+++ demo1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
@@ -12,4 +13,11 @@ func main() {
 	//按溢出处理
 	var num2 int8 = int8(num1)
 	fmt.Println(num2)
+	//数据类型转换成字符串
+	var str1 string = fmt.Sprintf("%d", i)
+	//方式一
+	fmt.Printf("str type %T str=%q\n", str1, str1)
+	//方式二 strconv
+	var str2 string = strconv.FormatInt(int64(i), 2)
+	fmt.Printf("str type %T str=%q\n", str2, str2)
 }
