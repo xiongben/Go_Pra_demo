71bf6adc2147ce054fe6bfa92f910f9bf4af31cc
xiongben <xiongben1993@gmail.com>
2020-02-26 16:51:35 +0800

    [add]

--- btyeDemo/main.go
+++ btyeDemo/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"awesomeProject1/btyeDemo/count"
 	"fmt"
 )
 
@@ -11,24 +10,37 @@ func init() {
 }
 
 func main() {
-	var i int = 5
-	fmt.Printf("%b", i)
+	//var i int = 5
+	//fmt.Printf("%b", i)
+	//
+	////8进制，0开头
+	//var j int = 011
+	//fmt.Println("j=", j)
+	//
+	////16进制，0x或0X开头表示
+	//var k int = 0x11
+	//fmt.Println("k=", k)
+	//
+	////位运算测试
+	//fmt.Println(2&3, 2|3, 2^3)
+	//fmt.Println(-2 & 3)
+	//fmt.Println(1>>2, 1<<2)
+	//
+	//count.Count()
+	//res1 := count.Sum(10, 1, 2, 3, 4, 5)
+	//fmt.Println(res1)
+	//fmt.Println(count.Age)
 
-	//8进制，0开头
-	var j int = 011
-	fmt.Println("j=", j)
+	//闭包问题
+	f := addUpper()
+	fmt.Println(f(1))
+	fmt.Println(f(2))
+}
 
-	//16进制，0x或0X开头表示
-	var k int = 0x11
-	fmt.Println("k=", k)
-
-	//位运算测试
-	fmt.Println(2&3, 2|3, 2^3)
-	fmt.Println(-2 & 3)
-	fmt.Println(1>>2, 1<<2)
-
-	count.Count()
-	res1 := count.Sum(10, 1, 2, 3, 4, 5)
-	fmt.Println(res1)
-	fmt.Println(count.Age)
+func addUpper() func(int) int {
+	var n int = 10
+	return func(i int) int {
+		n = n + i
+		return n
+	}
 }
