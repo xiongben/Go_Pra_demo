6a51b4ce43292c82282777f4d7a90b9560a354e1
Xiong Ben <xiongben1993@gmail.com>
2020-03-05 23:25:34 +0800

    [add]interface

--- interfaceDemo/demo1/main.go
+++ interfaceDemo/demo1/main.go
@@ -1,9 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+)
+
+type Hero struct {
+	Name string
+	Age  int
+}
+
+type HeroSlice []Hero
+
+//实现interface接口
+func (hs HeroSlice) Len() int {
+	return len(hs)
+}
+
+//从小到大排列
+func (hs HeroSlice) Less(i, j int) bool {
+	return hs[i].Age < hs[j].Age
+}
+
+func (hs HeroSlice) Swap(i, j int) {
+	//temp := hs[i]
+	//hs[i] = hs[j]
+	//hs[j] = temp
+	hs[i], hs[j] = hs[j], hs[i]
+}
 
 func main() {
 
-	var intSlice = []int{0, -1, 3, 5, 2, 8, 6, 60, 34}
-	fmt.Println(intSlice)
+	//var intSlice = []int{0, -1, 3, 5, 2, 8, 6, 60, 34}
+	//fmt.Println(intSlice)
+
+	var heros HeroSlice
+	for i := 0; i < 10; i++ {
+		hero := Hero{
+			Name: "heroxxx",
+			Age:  rand.Intn(100),
+		}
+		heros = append(heros, hero)
+	}
+
+	//排序前
+	for _, v := range heros {
+		fmt.Println(v)
+	}
+
+	sort.Sort(heros)
+	fmt.Println("=====================")
+	//排序后
+	for _, v := range heros {
+		fmt.Println(v)
+	}
 }
