505c180259283a5838c8b4ed72f104d3818a664b
xiongben <xiongben1993@gmail.com>
2020-07-08 12:26:31 +0800

    [add] linked list

--- Algorithms/linkedlist/Josepfu.go
+++ Algorithms/linkedlist/Josepfu.go
@@ -1,1 +1,24 @@
 package linkedlist
+
+type CircleSingleLinkedList struct {
+}
+
+type Boy struct {
+	no   int
+	next *Boy
+}
+
+func (this *Boy) getNo() int {
+	return this.no
+}
+
+func (this *Boy) setNo(no int) {
+	this.no = no
+}
+
+func (this *Boy) getNext() *Boy {
+	return this.next
+}
+func (this *Boy) setNext(next *Boy) {
+	this.next = next
+}
