e9807a688449e8d92bed3459396358e2475069d8
xiongben <xiongben1993@gmail.com>
2020-07-09 17:43:57 +0800

    [add] linked list

--- Algorithms/linkedlist/Josepfu.go
+++ Algorithms/linkedlist/Josepfu.go
@@ -1,6 +1,40 @@
 package linkedlist
 
+import "fmt"
+
 type CircleSingleLinkedList struct {
+	first *Boy
+}
+
+func (this *CircleSingleLinkedList) addBoy(nums int) {
+	if nums < 1 {
+		fmt.Println("输入的值不正确")
+		return
+	}
+	var curBoy *Boy
+	for i := 1; i <= nums; i++ {
+		boy := Boy{
+			no:   i,
+			next: nil,
+		}
+		if i == 1 {
+			this.first = &boy
+			this.first.setNext(this.first)
+			curBoy = this.first
+		} else {
+			curBoy.setNext(&boy)
+			boy.setNext(this.first)
+			curBoy = &boy
+		}
+	}
+}
+
+func (this *CircleSingleLinkedList) showBoy() {
+
+}
+
+func (this *CircleSingleLinkedList) countBoy() {
+
 }
 
 type Boy struct {
