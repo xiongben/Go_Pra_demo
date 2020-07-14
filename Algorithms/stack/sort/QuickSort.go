e4d840c7de326417d3b29e9d7f0a4ef99066e7df
Xiong Ben <xiongben1993@gmail.com>
2020-07-14 19:10:16 +0800

    [add] stack

--- Algorithms/stack/sort/QuickSort.go
+++ Algorithms/stack/sort/QuickSort.go
@@ -1,1 +1,25 @@
 package sort
+
+func quickSort(arr []int, left int, right int) {
+	l := left
+	r := right
+	pivot := arr[l]
+	temp := 0
+	for l != r {
+		for arr[l] < pivot && l < r {
+			l += 1
+		}
+		for arr[r] > pivot && l < r {
+			r -= 1
+		}
+		if l < r {
+			temp = arr[l]
+			arr[l] = arr[r]
+			arr[r] = temp
+		}
+	}
+	arr[left] = arr[l]
+	arr[l] = pivot
+	quickSort(arr, left, l-1)
+	quickSort(arr, l+1, right)
+}
