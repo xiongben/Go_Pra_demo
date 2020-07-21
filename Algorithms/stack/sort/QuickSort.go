e9a7bd354f5015386bed58a0573ab2fb168bb541
Xiong Ben <xiongben1993@gmail.com>
2020-07-21 16:08:30 +0800

    [add] sort

--- Algorithms/stack/sort/QuickSort.go
+++ Algorithms/stack/sort/QuickSort.go
@@ -3,23 +3,31 @@ package sort
 import "fmt"
 
 func QuickSortDemo() {
-	arr1 := []int{23, 45, 12, 66, 24, 89, 54, 11, 80, 2, 5, 77, 32}
-	quickSort(arr1, 0, 12)
+	arr1 := []int{4, 7, 2, 5, 77, 33, 89, 12, 3, 1}
+	quickSort(arr1, 0, len(arr1)-1)
 	fmt.Println(arr1)
+	//testfor()
 }
 
 func quickSort(arr []int, left int, right int) {
 	l := left
 	r := right
-	pivot := arr[l]
+	if left > right {
+		return
+	}
+	pivot := arr[left]
 	temp := 0
+	fmt.Println(pivot)
 	for l != r {
-		if arr[l] < pivot && l < r {
-			l += 1
+		for arr[r] >= pivot && l < r {
+			r--
 		}
-		if arr[r] > pivot && l < r {
-			r -= 1
+		for arr[l] <= pivot && l < r {
+			l++
 		}
+		//fmt.Println("l=",l)
+
+		fmt.Println(l, r)
 		if l < r {
 			temp = arr[l]
 			arr[l] = arr[r]
@@ -28,6 +36,22 @@ func quickSort(arr []int, left int, right int) {
 	}
 	arr[left] = arr[l]
 	arr[l] = pivot
+	//fmt.Println(arr)
 	quickSort(arr, left, l-1)
 	quickSort(arr, l+1, right)
+	return
+}
+
+func testfor() {
+	i := 1
+	j := 15
+	k := 10
+	for i < k {
+		i++
+
+	}
+	for j > k {
+		j--
+	}
+	fmt.Println(i, j)
 }
