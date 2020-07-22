4a2258eef0d340fafd31d6fc65ce324218042b9b
Xiong Ben <xiongben1993@gmail.com>
2020-07-22 23:57:04 +0800

    [add] sort

---
+++ Algorithms/stack/sort/radixSort.go
@@ -0,0 +1,16 @@
+package sort
+
+func radixSort(arr []int) {
+	max := arr[0]
+	arrlength := len(arr)
+	//得到数组中最大的数
+	for i := 1; i < len(arr); i++ {
+		if arr[i] > max {
+			max = arr[i]
+		}
+	}
+	//得到最大的数是几位数
+	maxLength := len(string(max))
+	bucket := make([][10]int, arrlength)
+	var bucketElementCounts [10]int
+}
