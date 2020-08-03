9a59c5638e54e5ec09eab5e40634e3228aba44c6
Xiong Ben <xiongben1993@gmail.com>
2020-08-03 18:53:50 +0800

    [add] recursion demo

--- Algorithms/stack/recursion/migong.go
+++ Algorithms/stack/recursion/migong.go
@@ -17,13 +17,28 @@ func MigongDemo() {
 	map1[3][1] = 1
 	map1[3][2] = 1
 	//输出地图
+	//for i := 0; i < 8; i++ {
+	//	for j := 0; j < 7; j++ {
+	//		fmt.Print(map1[i][j], " ")
+	//	}
+	//	fmt.Print("\n")
+	//}
+	setWay(&map1, 1, 1)
+	printMap(map1)
+	//arr2 := [...]int{1,1}
+	//testArr(&arr2)
+	//fmt.Println(arr2)
+
+}
+
+func printMap(map1 [8][7]int) {
+	//输出地图
 	for i := 0; i < 8; i++ {
 		for j := 0; j < 7; j++ {
 			fmt.Print(map1[i][j], " ")
 		}
 		fmt.Print("\n")
 	}
-
 }
 
 //使用递归方法给小球找路
@@ -33,6 +48,35 @@ func MigongDemo() {
 //约定：map[i][j]为0表示该点没有走过，为1表示墙，2表示通路可以走，3表示该点已经走过但是走不通
 //走的策略，下右上左
 
-func setWay(mapArr [][]int, i int, j int) bool {
+func setWay(mapArr *[8][7]int, i int, j int) bool {
+	if mapArr[6][5] == 2 {
+		return true
+	} else {
+		if mapArr[i][j] == 0 {
+			mapArr[i][j] = 2
+			if setWay(mapArr, i+1, j) {
+				return true
+			} else if setWay(mapArr, i, j+1) {
+				return true
+			} else if setWay(mapArr, i-1, j) {
+				return true
+			} else if setWay(mapArr, i, j-1) {
+				return true
+			} else {
+				mapArr[i][j] = 3
+				return false
+			}
+		} else {
+			return false
+		}
 
+	}
+
+}
+
+func testArr(targetarr *[2]int) {
+	targetarr[1] = 999
+	fmt.Println("=========")
+	fmt.Println(targetarr)
+	fmt.Println("=========")
 }
