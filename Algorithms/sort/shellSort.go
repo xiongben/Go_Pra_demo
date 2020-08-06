d566becfbf77009becae589edc06f70f3cb235fa
xiongben <xiongben1993@gmail.com>
2020-08-06 18:42:53 +0800

    [add] sort func

--- Algorithms/sort/shellSort.go
+++ Algorithms/sort/shellSort.go
@@ -4,9 +4,10 @@ import "fmt"
 
 func ShellSortDemo() {
 	arr1 := []int{8, 9, 1, 7, 2, 3, 5, 4, 6, 0}
-	shellSort(arr1)
+	shellSort2(arr1)
 }
 
+//交换法
 func shellSort(arr []int) {
 	//gap为步长
 	for gap := len(arr) / 2; gap > 0; gap /= 2 {
@@ -17,6 +18,25 @@ func shellSort(arr []int) {
 					arr[j] = arr[j+gap]
 					arr[j+gap] = temp
 				}
+			}
+		}
+	}
+	fmt.Println(arr)
+}
+
+//移动法
+func shellSort2(arr []int) {
+	//gap为步长
+	for gap := len(arr) / 2; gap > 0; gap /= 2 {
+		for i := gap; i < len(arr); i++ {
+			j := i
+			temp := arr[j]
+			if arr[j] < arr[j-gap] {
+				for j-gap >= 0 && temp < arr[j-gap] {
+					arr[j] = arr[j-gap]
+					j -= gap
+				}
+				arr[j] = temp
 			}
 		}
 	}
